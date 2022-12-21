468f54fd3e8182ee2cd1c9e3ec0f65ec708a102c
ThatGuyJop <joptelefoon@gmail.com>
2022-12-21 20:23:33 +0100

    Added jobs column

--- run.sql
+++ run.sql
@@ -2,9 +2,11 @@ CREATE TABLE IF NOT EXISTS `op-vehkeys` (
   `plate` varchar(50) NOT NULL,
   `identifier` varchar(255) NULL,
   `key_combo` varchar(50) NULL,
+  `jobs` longtext NULL,
   KEY `identifier` (`identifier`),
   KEY `key_combo` (`key_combo`),
-  KEY `plate` (`plate`)
+  KEY `plate` (`plate`),
+  KEY `jobs` (`jobs`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `op-vehlock` (
